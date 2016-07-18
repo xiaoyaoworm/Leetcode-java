d98f432f8da7f2fe5f6e5793169d4f19e8bc7a82
Yunlong Jiang <yunlong.jiang@elotouch.com>
2016-07-18 16:39:36 -0700

    update

--- 24_swipePairs.java
+++ 24_swipePairs.java
@@ -1,3 +1,4 @@
+Recursive解法：O(N) space:
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -9,20 +10,43 @@
 public class Solution {
     public ListNode swapPairs(ListNode head) {
         if(head == null || head.next == null) return head;
-        ListNode fake = new ListNode(0);
-        ListNode pre = fake;
+        ListNode second = head.next;
+        ListNode third = second.next;
+        second.next = head;
+        head.next = swapPairs(third);
+        return second;
+    }
+}
+
+
+Iterative解法：O(1) space
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode fakeHead = new ListNode(0);
+        fakeHead.next = head;
+        
+        ListNode pre = fakeHead;
         ListNode cur = head;
         
         while(cur!=null && cur.next!=null){
             ListNode next = cur.next.next;
-            cur.next.next = cur;
             pre.next = cur.next;
+            cur.next.next = cur;
             cur.next = next;
+            
             pre = cur;
             cur = next;
-            if(cur == null || cur.next == null) pre.next = cur;
+            if(cur==null || cur.next == null) pre.next = cur;
         }
-        
-        return fake.next;
+        return fakeHead.next;
     }
 }
