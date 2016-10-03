d60143f08631216e2b8eb8a29dd3d2b83e773434
Leon Jiang <xiaoyaoworm@gmail.com>
2016-10-03 01:25:24 -0700

    update one

---
+++ 201_rangeBitwiseAnd.java
@@ -0,0 +1,20 @@
+//See it from here: https://discuss.leetcode.com/topic/12133/bit-operation-solution-java/8
+
+// last bit of (odd number & even number) is 0.
+// when m != n, There is at least an odd number and an even number, so the last bit position result is 0.
+// Move m and n rigth a position.
+// Keep doing step 1,2,3 until m equal to n, use a factor to record the iteration time.
+
+//m<<step means make it left several and put 0 after it.
+
+public class Solution {
+    public int rangeBitwiseAnd(int m, int n) {
+        int step = 0;
+        while(m!=n){
+            m >>= 1; // dont forget that =
+            n >>= 1;
+            step++;
+        }
+        return m<<step;
+    }
+}
