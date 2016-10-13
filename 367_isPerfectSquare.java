80c2a5817718b326b7a787b1b72985cb87856c1f
Leon Jiang <xiaoyaoworm@gmail.com>
2016-10-13 02:42:29 -0700

    update one

---
+++ 367_isPerfectSquare.java
@@ -0,0 +1,16 @@
+//O(logN)解法！
+public class Solution {
+    public boolean isPerfectSquare(int num) {
+        if(num < 0) return false;
+        int i = 1; //start from 1 not 0!!!
+        int j = num;
+        long mid = 0; //define as long will solve lots of overflow problems!!!!
+        while(i <= j){
+            mid = i+(j-i)/2;
+            if(mid * mid == num) return true;
+            else if(mid * mid < num) i = (int)mid+1; //dont forget (int) convert
+            else j = (int)mid-1;
+        }
+        return false;
+    }
+}
