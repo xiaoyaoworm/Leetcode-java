385ac1fe0a84e6c9786375701d4159d0c438995c
Leon Jiang <xiaoyaoworm@gmail.com>
2016-10-13 02:53:29 -0700

    update one

--- 367_isPerfectSquare.java
+++ 367_isPerfectSquare.java
@@ -4,7 +4,7 @@ public class Solution {
         if(num < 0) return false;
         int i = 1; //start from 1 not 0!!!
         int j = num;
-        long mid = 0; //define as long will solve lots of overflow problems!!!!
+        long mid = 0; //define as long will solve lots of overflow problems!!!! mid * mid may overflow!!!
         while(i <= j){
             mid = i+(j-i)/2;
             if(mid * mid == num) return true;
