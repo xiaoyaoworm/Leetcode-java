5bd818442a7a02bf319d6b1e92c3e8d0308b269b
xiaoyaoworm <xiaoyaoworm@gmail.com>
2021-10-22 20:50:34 -0700

    update

--- 34_searchRange.java
+++ 34_searchRange.java
@@ -25,3 +25,44 @@ public class Solution {
         return result;
     }
 }
+
+//更好理解
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int left = findBound(nums, target, true);
+        if(left == -1) return new int[]{-1,-1};
+        int right = findBound(nums, target, false);
+        return new int[]{left, right};
+    }
+    
+    private int findBound(int[] nums, int target, boolean isLeft){
+        int i = 0;
+        int j = nums.length-1;
+        
+        while(i <= j){
+            int mid = i+(j-i)/2;
+            if(nums[mid] == target){
+                if(isLeft){
+                    //Find the left boundry;
+                    if(mid == i || nums[mid-1]!=nums[mid]){
+                        return mid;
+                    } else{
+                        j = mid-1;
+                    }
+                } else{
+                    // Find the right boundry;
+                    if(mid == j || nums[mid+1]!=nums[mid]){
+                        return mid;
+                    } else{
+                        i = mid+1;
+                    }
+                }
+            } else if(nums[mid] > target){
+                j = mid-1;
+            } else{
+                i = mid+1;
+            }
+        }
+        return -1;
+    }
+}
